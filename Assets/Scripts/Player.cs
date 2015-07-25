673ef3006f850737a3c9d7e95615d90676f82581
ABeloeil <abeloeil@u-pro.fr>
2015-07-25 16:02:43 +0200

    add audio effects

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,13 @@ public class Player : MovingObject
 	public int pointsPerSoda = 20;
 	public float restartLevelDelay = 1f;
 	public Text foodText;
+	public AudioClip moveSound1;
+	public AudioClip moveSound2;
+	public AudioClip eatSound1;
+	public AudioClip eatSound2;
+	public AudioClip drinkSound1;
+	public AudioClip drinkSound2;
+	public AudioClip GameOverSound;
 
 	private Animator animator;
 	private int food;
@@ -53,10 +60,12 @@ public class Player : MovingObject
 		} else if (other.tag == "Food") {
 			food += pointsPerFood;
 			foodText.text = "+" + pointsPerFood + " Food: " + food;
+			SoundManager.instance.RandomizeSfx(eatSound1, eatSound2);
 			other.gameObject.SetActive(false);
 		} else if (other.tag == "Soda") {
 			food += pointsPerSoda;
 			foodText.text = "+" + pointsPerSoda + " Food: " + food;
+			SoundManager.instance.RandomizeSfx(drinkSound2, drinkSound2);
 			other.gameObject.SetActive(false);
 		}
 	}
@@ -89,13 +98,21 @@ public class Player : MovingObject
 		base.AttemptMove<T> (xDir, yDir);
 
 		RaycastHit2D hit;
+		if (Move(xDir,yDir,out hit))
+	    {
+			SoundManager.instance.RandomizeSfx(moveSound1, moveSound2);
+		}
+
 		CheckIfGameOver ();
 		GameManager.instance.playersTurn = false;
 	}
 
 	private void CheckIfGameOver()
 	{
-		if (food <= 0)
+		if (food <= 0) {
+			SoundManager.instance.PlaySingle(GameOverSound);
+			SoundManager.instance.musicSource.Stop();
 			GameManager.instance.GameOver ();
+		}
 	}
 }
