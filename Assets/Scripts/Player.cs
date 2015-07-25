020b994aa38354eed7a31856dac07dac3984b157
ABeloeil <abeloeil@u-pro.fr>
2015-07-25 14:46:02 +0200

    update enemy1 animation controller, add UI text, update enemies prefabs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Player : MovingObject 
 {
@@ -7,6 +8,7 @@ public class Player : MovingObject
 	public int pointsPerFood = 10;
 	public int pointsPerSoda = 20;
 	public float restartLevelDelay = 1f;
+	public Text foodText;
 
 	private Animator animator;
 	private int food;
@@ -15,6 +17,7 @@ public class Player : MovingObject
 	{
 		animator = GetComponent<Animator> ();
 		food = GameManager.instance.playerFoodPoints;
+		foodText.text = "Food: " + food;
 
 		base.Start();
 	}
@@ -49,9 +52,11 @@ public class Player : MovingObject
 			enabled = false;
 		} else if (other.tag == "Food") {
 			food += pointsPerFood;
+			foodText.text = "+" + pointsPerFood + " Food: " + food;
 			other.gameObject.SetActive(false);
 		} else if (other.tag == "Soda") {
 			food += pointsPerSoda;
+			foodText.text = "+" + pointsPerSoda + " Food: " + food;
 			other.gameObject.SetActive(false);
 		}
 	}
@@ -72,12 +77,14 @@ public class Player : MovingObject
 	{
 		animator.SetTrigger ("playerHit");
 		food -= loss;
+		foodText.text = "-" + loss + " Food: " + food;
 		CheckIfGameOver ();
 	}
 
 	protected override void AttemptMove<T> (int xDir, int yDir)
 	{
 		food--;
+		foodText.text = "Food: " + food;
 
 		base.AttemptMove<T> (xDir, yDir);
 
@@ -88,7 +95,7 @@ public class Player : MovingObject
 
 	private void CheckIfGameOver()
 	{
-		/*if (food <= 0)
-			GameManager.instance.GameOver ();*/
+		if (food <= 0)
+			GameManager.instance.GameOver ();
 	}
 }
